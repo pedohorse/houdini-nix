0a882dcba068688a888e050da7528c1eef3f5a51
pedohorse <13556996+pedohorse@users.noreply.github.com>
2023-11-07 12:24:40 +0100

    fix defaults

--- runtime.nix
+++ runtime.nix
@@ -3,11 +3,11 @@
 let
   license_dir = "~/.config/houdini";
 in
-callPackage ./runtime-build.nix {
+callPackage ./runtime-build.nix rec {
   version = "19.5.569";
   src = requireFile rec {
     name = "houdini-${version}-linux_x86_64_gcc9.3.tar.gz";
     sha256 = "0c2d6a31c24f5e7229498af6c3a7cdf81242501d7a0792e4c33b53a898d4999e";
-    url = meta.homepage;
+    url = "https://www.sidefx.com/download/daily-builds/?production=true";
   };
-};
+}
